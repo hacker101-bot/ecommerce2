d27d00428c3c8cac41964333647e533bdabbfcdb
unknown <chrysolodivine@gmail.com>
2025-11-04 10:18:35 +0100

    Added these files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './Pages/HomePage';
-import { CheckoutPage } from './Pages/CheckoutPage';
+import { CheckoutPage } from './Pages/checkout/CheckoutPage';
 import { OrdersPage } from './Pages/OrdersPage';
 import {TrackingPage} from './Pages/TrackingPage'
 import './App.css';
